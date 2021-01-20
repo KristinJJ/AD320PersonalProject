e5e490932eb343099f07745c6379d58e97f3adb8
Kristin Jue <kristin.jue@gmail.com>
2021-01-19 23:33:34 -0800

    updated GET request to return JSON values in an HTML page, and updated POST request to return HTML page with list of names

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
+"use strict"
+const { read } = require('fs');
 const http = require('http');
 
 const requestListener = function( req, res) {
-    if (req.url == "/") {
+    let users = [
+        { name: 'Eric', status: 'admin'},
+        { name: 'Jessica', status: 'admin'},
+        { name: 'Chad', status: 'client'}
+    ];
+
+    /*req.on('data', chunk => {
+        console.log(`Data chunk available: ${chunk}`)
+      })*/
+      
+    if (req.url == '/' && req.method == 'POST') {
         // set response header
-        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.writeHead(200, {'Content-Type': 'application/json'});
 
         // set response content
-        res.write('<html><body><p>This is the home page.</p></body></html>');
+        res.write(`<h3>${JSON.stringify(users)}</h3>`);
         res.end();
 
-    } else if (req.url == "/users") {
+    } else if (req.url == '/users' && req.method == 'GET') {
         // set response header
         res.writeHead(200, {'Content-Type': 'text/html'});
 
         // set response content
-        res.write('<html><body><p>This is the users page.</p><ol><li>Eric</li><li>Jessica</li><li>Chad</li></ol></body></html>');
-        res.end();
+        let userNames = "<ol>";
+        let findUserNames = users.find(function(user, index) {
+            userNames += "<li>" + user.name + "</li>";
+        }) 
+        userNames += "</ol>";
 
+        res.write(`<html><body><p>This is the list of user names.${userNames}</p></body></html>`);
+        res.end();
+        
     } else {
         res.end('Invalid Request.');
     }
 }
+
 
 // create the web server
 const server = http.createServer(requestListener);
