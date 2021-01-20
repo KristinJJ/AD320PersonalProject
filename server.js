02e596b08e46eb2219dbe99444b4ad4070a54844
Kristin Jue <kristin.jue@gmail.com>
2021-01-19 16:39:39 -0800

    change test message to HTML list of names

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const http = require('http');
 
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    res.end('HelloWorld!');
+const requestListener = function( req, res) {
+    if (req.url == "/") {
+        // set response header
+        res.writeHead(200, {'Content-Type': 'text/html'})
+
+        // set response content
+        res.write('<html><body><p>This is the home page.</p></body></html>');
+        res.end();
+
+    } else if (req.url == "/users") {
+        // set response header
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        // set response content
+        res.write('<html><body><p>This is the users page.</p><ol><li>Eric</li><li>Jessica</li><li>Chad</li></ol></body></html>');
+        res.end();
+
+    } else {
+        res.end('Invalid Request.');
+    }
 }
 
+// create the web server
 const server = http.createServer(requestListener);
-server.listen(8080);
+
+// listen for incoming requests
+server.listen(5000); 
+
+console.log('Node.js web server at port 5000 is running! :)');
