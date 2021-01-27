67fa3fb8e35aed33cb6cd16a15d36cb0f0344c41
Kristin Jue <kristin.jue@gmail.com>
2021-01-26 23:23:18 -0800

    Added logo image to HomePage.js

---
+++ client/src/HomePage.js
@@ -0,0 +1,12 @@
+import logo from './images/picture.png';
+//import '.HomePage.css';
+
+function App() {
+    return (
+        <div className="HomePage">
+            <img src={logo} className="HomePageLogo" alt="logo"/>
+        </div>
+    )
+}
+
+export default App;
